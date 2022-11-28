93f4dcbd2344447a5bf8aac2bbcd52f43ee26a07
Sayooj S Kiran <sayooj.kiran@QUALITEST.LOCAL>
2022-11-28 18:11:45 +0530

    changing the number of testscases

---
+++ cypress/Integration/Test/csv.js
@@ -0,0 +1,10 @@
+///<reference types="cypress"/>
+let csvToJson = require('convert-csv-to-json');
+describe("my first csv retreiving",()=>{
+    it("csv data",()=>{
+        cy.visit("https://github.com/");
+
+    })
+})
+
+
